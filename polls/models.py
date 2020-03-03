df8c8078383a250a1de7ffbd68c7e5cf7738d0e4
Manuel Velez Martinez <m.martinez@fashionette.de>
2020-03-03 14:49:39 +0100

    new stuff

--- polls/models.py
+++ polls/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
-# Create your models here.
-
+# the following lines added:
 import datetime
 from django.utils import timezone
 
-class Questions(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class Question(models.Model):
+   question_text = models.CharField(max_length=200)
+   pub_date = models.DateTimeField('date published')
 
    def __str__(self):
        return self.question_text
